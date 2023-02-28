a53300433d4bffa81fbde224b432f71432c26ea7
youngcheol <zodiac0905@gmail.com>
2023-02-28 23:35:34 +0900

    Add Type Alias (type.ts)

---
+++ type.ts
@@ -0,0 +1,41 @@
+// 타입 앨리어스
+type Person = {
+    name: string,
+    age?: number
+}
+
+// 빈 객체를 Person 타입으로 지정
+const person = {} as Person;
+person.name = 'Lee';
+person.age = 20;
+//   person.address = 'Seoul'; // Error
+
+
+// 문자열 리터럴로 타입 지정
+type Str = 'Lee';
+
+// 유니온 타입으로 타입 지정
+type Union = string | null;
+
+// 문자열 유니온 타입으로 타입 지정
+type Name = 'Lee' | 'Kim';
+
+// 숫자 리터럴 유니온 타입으로 타입 지정
+type Num = 1 | 2 | 3 | 4 | 5;
+
+// 객체 리터럴 유니온 타입으로 타입 지정
+type Obj = { a: 1 } | { b: 2 };
+
+// 함수 유니온 타입으로 타입 지정
+type Func = (() => string) | (() => void);
+
+// 인터페이스 유니온 타입으로 타입 지정
+type Square = number;
+type Rectangle = string;
+type Circle = boolean;
+
+type Shape = Square | Rectangle | Circle;
+
+// 튜플로 타입 지정
+type Tuple = [string, boolean];
+// const t: Tuple = ['', '']; // Error
